d6669cda1d9297484d2475a949020193c5bed680
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2021-08-12 06:20:55 -0400

    Built copy on landing page

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/index.module.css";
 
 const index = () => {
   return (
-    <div>
-      hey
+    <div id={styles.landingContainer}>
+      <div id={styles.copyContainer}>
+        <h1>
+          Build <span className={styles.fireH1}>powerful</span> web experiences.
+        </h1>
+        <p>
+          Hi—I’m <strong>Rocco Maniscalco</strong>. I’m a full stack developer
+          leveraging modern web technologies to build applications that deliver
+          impact.
+        </p>
+      </div>
     </div>
   );
 };
