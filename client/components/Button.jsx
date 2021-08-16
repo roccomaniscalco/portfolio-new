b87ed36e7c07f82159543ca89b0491b34f529280
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2021-08-16 06:56:23 -0400

    Created perspective hover effect for buttons

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,14 +1,26 @@
 import styles from "../styles/components/button.module.css";
+import useCursor from "../customHooks/useCursor";
 
 const Button = ({ primary, secondary, handleClick, children }) => {
+  const [cursor, targetEl] = useCursor();
+
   const switchStyles = () => {
     if (primary) return styles.btnPrimary;
     if (secondary) return styles.btnSecondary;
   };
 
   return (
-    <button className={`${styles.btn} ${switchStyles()}`} onClick={handleClick}>
-      {children}
+    <button
+      className={`${styles.btn} ${switchStyles()}`}
+      onClick={handleClick}
+      ref={targetEl}
+      style={{
+        transform: `perspective(${cursor.clientWidth}px) 
+        rotateX(${cursor.rotationX}deg) 
+        rotateY(${cursor.rotationY}deg)`,
+      }}
+    >
+      <div className={styles.btnContent}>{children}</div>
     </button>
   );
 };
--- client/customHooks/useCursor.js
+++ client/customHooks/useCursor.js
@@ -1,20 +1,39 @@
 import { useState, useRef, useEffect } from "react";
 
 const useCursor = () => {
-  const [cursor, setCursor] = useState({ clientX: 0, clientY: 0 });
+  const DEFAULT_CURSOR = {
+    clientWidth: 0,
+    clientHeight: 0,
+    clientX: 0,
+    clientY: 0,
+    rotationX: 0,
+    rotationY: 0,
+  };
+
+  const [cursor, setCursor] = useState(DEFAULT_CURSOR);
   const targetEl = useRef(null);
 
   const handleMouseMove = (e) => {
     const { clientWidth, clientHeight } = targetEl.current;
-    const { clientX, clientY } = e;
+    const { clientX, clientY, layerX, layerY } = e;
+
     setCursor({
-      clientX: parseInt(-clientWidth / 2 + clientX),
-      clientY: parseInt(clientHeight / 2 - clientY),
+      clientX: clientX - clientWidth / 2,
+      clientY: clientY - clientHeight / 2,
+      rotationX: -20 * ((layerY - clientHeight / 2) / clientHeight),
+      rotationY: 20 * ((layerX - clientWidth / 2) / clientWidth),
+      clientWidth: clientWidth,
+      clientHeight: clientHeight,
     });
+  };
+
+  const handleMouseLeave = () => {
+    setCursor(DEFAULT_CURSOR);
   };
 
   useEffect(() => {
     targetEl.current.onmousemove = (e) => handleMouseMove(e);
+    targetEl.current.onmouseleave = () => handleMouseLeave();
   }, [cursor]);
 
   return [cursor, targetEl];
